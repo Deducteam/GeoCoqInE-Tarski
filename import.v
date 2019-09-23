325960d7b0eefb237abece6cc11b549e2a8f749d
Pierre Boutry <boutry@eduroam-093b.sophia.inria.fr>
2019-09-23 11:56:16 +0200

    some progress

--- import.v
+++ import.v
@@ -1,4 +1,3 @@
-
 (* Small sample to check Makefile *)
 (*
 Require Import GeoCoq.Utils.general_tactics.
@@ -8,22 +7,4 @@ Require Import GeoCoq.Tactics.finish.
 Require Import GeoCoq.Tarski_dev.Ch02_cong.
 *)
 
-Require Import GeoCoq.Utils.general_tactics.
-Require Import GeoCoq.Axioms.tarski_axioms.
-Require Import GeoCoq.Tarski_dev.Definitions.
-Require Import GeoCoq.Tactics.finish.
-Require Import GeoCoq.Tarski_dev.Ch02_cong.
-Require Import GeoCoq.Tarski_dev.Ch03_bet.
-Require Import GeoCoq.Tarski_dev.Ch04_cong_bet.
-Require Import GeoCoq.Tarski_dev.Ch04_col.
-Require Import GeoCoq.Meta_theory.Decidability.equivalence_between_decidability_properties_of_basic_relations.
-Require Import GeoCoq.Tarski_dev.Ch05_bet_le.
-Require Import GeoCoq.Tarski_dev.Ch06_out_lines.
-Require Import GeoCoq.Tactics.Coinc.tactics_axioms.
-Require Import GeoCoq.Meta_theory.Models.tarski_to_col_theory.
-Require Import GeoCoq.Utils.TCSets.
-Require Import GeoCoq.Utils.sets.
-Require Import GeoCoq.Tactics.Coinc.ColR.
-Require Import GeoCoq.Tarski_dev.Tactics.ColR.
-Require Import GeoCoq.Tarski_dev.Ch07_midpoint.
-Require Import GeoCoq.Tarski_dev.Ch08_orthogonality.
+Require Import GeoCoq.Tarski_dev.Ch16_coordinates.
